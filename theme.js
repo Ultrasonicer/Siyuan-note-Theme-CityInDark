30078c6080c4c95c1d211afd6064792ae9b83375
weihan-Chen <weihan-Chen@qq.com>
2022-04-04 02:21:01 +0800

    v0.1.0 配色+透明化

---
+++ theme.js
@@ -0,0 +1,8 @@
+/*
+function loadScript(url, type = 'module') {
+    let script = document.createElement('script');
+    script.setAttribute('type', type);
+    script.setAttribute('src', url);
+    document.head.appendChild(script);
+}
+loadScript("https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js");*/
